3387e263226fb7c714d4f63dc3f5820aaeae1746
Muthuram-45 <muthuram921@gmail.com>
2025-01-30 22:08:24 +0530

    new footer

--- spoken/src/Component/Footer.jsx
+++ spoken/src/Component/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
-    <h1>hi</h1>
+    <div className='footer'>
+
+        <ul>
+            <li className='heading'>Address</li>
+            <li>3rd,Floor BMC Market,ThisAi</li>
+            <li>Books , Thisaiyanvilai,</li>
+            <li>Tirunelveli DT-627657</li>
+        </ul>
+        <ul >
+            <li className='heading'>Our Services</li>
+            <li>Speaching</li>
+            <li>Writting</li>
+            <li>Grammer</li>
+        </ul>
+        <ul>
+            <li className='heading'>Contact us</li>
+            <li><a href=''></a></li>
+            <li> nmgiuw</li>
+            <li>jgwiury</li>
+        </ul>
+        <ul>
+            <li className='heading'> bmc</li>
+            <li>123</li>
+            <li> nmgiuw</li>
+            <li>jgwiury</li>
+        </ul>
+
+    </div>
     </>
   )
 }
