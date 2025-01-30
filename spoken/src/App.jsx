d6e74106b0d5a5d31089a35a7084505d9a881233
Muthuram-45 <muthuram921@gmail.com>
2025-01-30 21:46:50 +0530

    footer

--- spoken/src/App.jsx
+++ spoken/src/App.jsx
@@ -1,16 +1,14 @@
 
 import './App.css'
+import { Footer } from './Component/Footer'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>hello welcome</h1>
-        <h2>some changes</h2>
-        <h3>hi</h3>
-        <h4>hey</h4>
-      </div>
+    
+        <Footer/>
+  
     </>
   )
 }
