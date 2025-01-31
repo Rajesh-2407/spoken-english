d0050ccd1827bb8bc01987521d2d11e960348e99
Madhavan6778 <madhavan6778@gmail.com>
2025-01-31 11:58:20 +0530

    new

--- spoken/src/App.jsx
+++ spoken/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Footer } from './Component/Footer'
+import { Nav } from './Component/Nav'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <>
     
         <Footer/>
-  
+        <Nav/>
     </>
   )
 }
