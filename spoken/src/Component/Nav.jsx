8070df0069a108419783f527488aadc3dc1c47f3
Madhavan6778 <madhavan6778@gmail.com>
2025-01-31 14:32:18 +0530

    new

--- spoken/src/Component/Nav.jsx
+++ spoken/src/Component/Nav.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import './nav.css'
 
 export const Nav = () => {
-  return (
-    <>
-    <div className="navbar">
-        <div className="navlogo">
-            <h2>ThisAI</h2>
-            <h4>spoken English</h4>
-        </div>
-        <div className="navlist">
-            <ul className='nav-ul'>
-                <li className='nav-li'>Home</li>
-                <li className='nav-li'>Components</li>
-                <li className='nav-li'>Chat</li>
-                <li className='nav-li'>Contact</li>
-            </ul>
-        </div>
-        <div className="navbutton">
-            <button className='nav-btn'><a href="">Sign In</a></button>
-        </div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="navbar">
+                <div className="navlogo">
+                    <h2>ThisAI</h2>
+                    <h4>spoken English</h4>
+                </div>
+                <div className="navlist">
+                    <ul className='nav-ul'>
+                        <li className='nav-li'><a href="">Home</a></li>
+                        <li className='nav-li'><a href="">Components</a></li>
+                        <li className='nav-li'><a href="">Chat</a></li>
+                        <li className='nav-li'><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="navbutton">
+                    <button className='nav-btn'><a href="">Sign In</a></button>
+                </div>
+            </div>
+        </>
+    )
 }
