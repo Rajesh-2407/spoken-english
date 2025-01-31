6e463f734410ce64af9c760e57b278c8c3c6b572
Muthuram-45 <muthuram921@gmail.com>
2025-01-31 12:01:01 +0530

    new

--- spoken/src/Component/Footer.jsx
+++ spoken/src/Component/Footer.jsx
@@ -25,19 +25,21 @@ export const Footer = () => {
         </ul>
         <ul>
             <li className='heading'>Contact us</li>
-            <li><a href=''><FaPhone />+91 6380683086</a></li>
-            <li>sales@thisaitech.com</li>
+            <li><a href=''>&nbsp<FaPhone />+91 6380683086</a></li>
+            <li><a href=''>sales@thisaitech.com</a></li>
         </ul>
-        <ul>
+        <ul >
             <li className='heading'>Follow</li>
-            <li>
+            <div className='icon'>
+            <li className='icon-one'>
                <span><FaSquareWhatsapp /></span> 
                <span><FaSquareInstagram /></span> 
             </li>
-            <li>
+            <li className='icon-two'>
                 <span><FaYoutube /></span>
                 <span><FaTwitter /></span>
             </li>
+            </div>
         </ul>
 
     </div>
