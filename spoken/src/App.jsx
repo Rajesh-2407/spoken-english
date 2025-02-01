ca1d4ad81e9013f6d1195d16b462a3415f86ce3a
Muthuram-45 <muthuram921@gmail.com>
2025-02-01 14:35:17 +0530

    Upload

--- spoken/src/App.jsx
+++ spoken/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import FileUploader from './Component/FileUploader'
 import { Footer } from './Component/Footer'
 import { Nav } from './Component/Nav'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
         <Nav/>
+        <FileUploader/>
         <Footer/>
         
     </>
